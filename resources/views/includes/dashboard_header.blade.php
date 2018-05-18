a05810ca83d13976837b8e19c2c4806437bc01c0
11ninjacoder <ninjacoder11@gmail.com>
2018-05-18 15:55:52 +0530

    deleted backup files

--- resources/views/includes/dashboard_header.blade.php
+++ resources/views/includes/dashboard_header.blade.php
@@ -1,3 +1,3 @@
-<div class="col-md-4">Paymate</div><div class="col-md-4"></div><div class="col-md-4 text-right"><div class="header-link"><a href="/logout">Log Out</a></div></div>
+<div class="col-md-4">Site Name</div><div class="col-md-4"></div><div class="col-md-4 text-right"><div class="header-link"><a href="/logout">Log Out</a></div></div>
 
     
